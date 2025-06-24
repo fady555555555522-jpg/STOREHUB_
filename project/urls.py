6bcb2a5a6bca009c93978cd53bc9891d21e62138
FADY ASHRAF <fadya5323@gmail.com>
2025-06-24 15:22:25 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .view import *
+#from .view import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +11,6 @@ urlpatterns = [
     path('api/', include('pages.api_urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = handler404
-handler500 = handler500
+#handler404 = handler404
+#handler500 = handler500
 
