78e6b14d520771897e267831a57ac4868db9b8b6
FADY ASHRAF <fadya5323@gmail.com>
2025-06-24 14:58:38 +0300

    Update views.py

--- project/views.py
+++ project/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 
 
-def handel404(request,exception):
-    return render(request, '400.html', status = 404)
+def handler404(request, exception):
+    return render(request, '400.html', status=404)
 
-def handel500(request,exception):
-    return render(request, '500.html', status = 500)
-
-def index(request):
-    return render(request, '400.html', name = 'index')
-
+def handler500(request):
+    return render(request, '500.html', status=500)
